1a0a891ee599e33886276a3bc41b4a17e466f52b
Nehak17 <nkharkar@mail.usf.edu>
2018-03-12 15:30:23 -0400

    commit

--- ModuleWorking/Routing/app.js
+++ ModuleWorking/Routing/app.js
@@ -2,11 +2,23 @@ var http=require('http');
 var fs=require('fs');
 
 http.createServer(function(req, res) {
+    if(req.url === '/')
+    {
+    fs.createReadStream(__dirname + '/index.htm').pipe(res) ;
+    }
+    
+    else if(req.url === '/api')
+    {
     res.writeHead(200,{'Content-Type':'application/json'});
-
     var obj={
-        firstname:'Neha',
-        lastname: 'Kharkar'
-    };
+            firstname:'Neha',
+            lastname: 'Kharkar'
+            };
     res.end(JSON.stringify(obj));
+    }
+    else{
+    res.writeHead(404);
+    res.end();
+    }
+    
 }).listen(1323,'127.0.0.1');
